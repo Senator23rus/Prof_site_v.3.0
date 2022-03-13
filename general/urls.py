4e27c973741757bdedf5790393556a67a69a3e00
senator23 <senator295@gmail.com>
2022-03-13 13:21:07 +0300

    add photo

--- general/urls.py
+++ general/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.title),
     path('news/', views.news, name='news'),
+    path('home/', views.title, name='home'),
 
     path('contact/', TemplateView.as_view(template_name="contact.html"), name="contact"),
     # path('contact/', TemplateView.as_view(template_name="contact.html"), name="contact"), филиалы
--- general/views.py
+++ general/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 def title(request):
     return render(request, 'index.html')
 
-
 def news(request):
     return render(request, 'news.html')
 
