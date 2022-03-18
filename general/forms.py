5cef4ceed7865f90fef91508eb395c87812040be
senator23 <senator295@gmail.com>
2022-03-18 21:38:27 +0300

    Work with form Connect us

---
+++ general/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class UserForm(forms.Form):
+    name = forms.CharField(label="Ваши ФИО", max_length=50)
+    e_mail = forms.EmailField(label="Ваш E-mail", max_length=30)
+    filial = forms.CharField(label='Филиал ПАО "Ростелеком"', max_length=20)
+    message = forms.CharField(label="Ваше обращение", widget=forms.Textarea)
+    required_css_class = "form1"
--- general/views.py
+++ general/views.py
@@ -1,10 +1,17 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .forms import UserForm
 
 
 
 
 def title(request):
-    return render(request, 'index.html')
+    userform = UserForm()
+    if request.method == "POST":
+        userform = UserForm(request.POST)
+        if userform.is_valid():
+            return HttpResponseRedirect(request.path)
+    return render(request, 'index.html', {"form": userform})
 
 def news(request):
     return render(request, 'news.html')
